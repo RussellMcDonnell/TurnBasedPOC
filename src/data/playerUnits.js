d06d93ca763dd3c34f96170bdb25a5d2de98443e
Russell McDonnell <russell.alan.mcdonnell@gmail.com>
2025-03-15 21:23:42 -0600

    Clean up

--- src/data/playerUnits.js
+++
@@ -1,8 +0,0 @@
-// Import unit portraits and full art
-import varenPortrait from "../assets/images/units/varen-stormrune-portrait-picture.png";
-import ashbringerPortrait from "../assets/images/units/ashbringer-portrait-picture.png";
-import lynValkenPortrait from "../assets/images/units/lyn-valken-portrait-picture.png";
-import emberhowlPortrait from "../assets/images/units/emberhowl-portrait-picture.png";
-import silkfangPortrait from "../assets/images/units/silkfang-portrait-picture.png";
-import varenFullArt from "../assets/images/units/varen-stormrune-full-art.png";
-
