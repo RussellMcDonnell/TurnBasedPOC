ec5abeb76038833c1f309cfbdfe47ab0aaa4580c
Russell McDonnell <russell.alan.mcdonnell@gmail.com>
2025-03-17 22:26:54 -0600

    Add new keywords with descriptions for enhanced gameplay mechanics

--- src/data/keywords.js
+++ src/data/keywords.js
@@ -1,6 +1,38 @@
 export const keywords = {
-    "varen": {
-      id: "Armored",
+    "Armored": {
       description: "Block (3) amount of damage from attacks."
+    },
+    "Blood Rage": {
+      description: "Gain (1) attack power each time you take damage. Blood rage ends between combat scenarios."
+    },
+    "Dual Wield": {
+      description: "When attacking, you deal your damage twice"
+    },
+    "Flank": {
+      description: "Bypass units with taunt, targetting enemies at will"
+    },
+    "Last Stand": {
+      description: "Trigger an effect upon death"
+    },
+    "Lifesteal": {
+      description: "Heals the unit based on the amount of damage dealt."
+    },
+    "Melee": {
+      description: "Unit takes retaliation damage when attacking."
+    },
+    "Poisonous": {
+      description: "Dealing damage applies poisonous status"
+    },
+    "Preemptive Strike": {
+      description: "Strike an enemy first. If they die, you take no retaliation damage."
+    },
+    "Ranged": {
+      description: "Unit doesn't take retaliation damage when attacking."
+    },
+    "Renew": {
+      description: "Restore (5) health at end of round. Renew removes burned and injured status effect at end of round, but does not provide healing that round."
+    },
+    "Taunt": {
+      description: "Enemies must attack units with Taunt."
     }
 }
